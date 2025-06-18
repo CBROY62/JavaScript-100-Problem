edc956033e921eef692046c03ce3ba187ce57869
CBROY62 <chandrabhushankumar27080@gmail.com>
2025-06-18 16:00:53 +0530

    create by hello word program

---
+++ hello.js
@@ -0,0 +1,10 @@
+let hello = "bhushan"
+const str = String(hello)
+console.log(str)
+ const number = hello.length
+if (number === 7 ) {
+console.log(" This is My First Program Hello Word ");
+   
+}else{
+    console.log(" Then this is a not print hello word")
+}
