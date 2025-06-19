46fcfa7703a2235ba0ce1dffbdf18de02356d147
CBROY62 <chandrabhushankumar27080@gmail.com>
2025-06-19 10:51:52 +0530

    some of data

---
+++ calculateRadious.js
@@ -0,0 +1,40 @@
+// // 1. Log calculation from a fixed value
+// const rad = 70;
+// const finalradius = rad / (2 * 3.14159);
+// console.log(finalradius);
+
+// // 2. Prompt user and alert calculated radius
+// const radiusInput = prompt("Enter the circumference to calculate radius:");
+// const calculatedRadius = radiusInput / (2 * 3.14159);
+// alert("Calculated radius is: " + calculatedRadius);
+
+// // 3. Show result in the UI using DOM
+// const select = document.getElementById('hed');
+// select.innerHTML = `${calculatedRadius}`;
+
+
+  //final value calculating //
+
+  
+
+  function submitFunction() {
+  // Access the input field
+  const inputsection = document.getElementsByClassName('inpute_secction')[0];
+
+  // Get the value from the input
+  const innerValueGet = inputsection.value;
+  console.log(innerValueGet);
+
+
+  const radious_value = innerValueGet/(2*3.14159);
+
+  // Access the heading and set its textContent
+  const heading_secction = document.getElementsByClassName('First_heading')[0];
+  heading_secction.textContent = ` claculating value ${radious_value}`;
+
+  console.log(heading_secction);
+}
+
+
+ 
+ 
