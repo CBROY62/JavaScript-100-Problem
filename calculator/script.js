bbdf859aae0f3a433d81ea2b78dd36ffec23c619
CBROY62 <chandrabhushankumar27080@gmail.com>
2025-06-30 19:13:17 +0530

    create a calculator for infini number of calculation

--- calculator/script.js
+++ calculator/script.js
@@ -1,8 +1,8 @@
-const num1 = BigInt(prompt())
-console.log(num1);
-const num2 = BigInt(prompt())
-console.log(num2)
-const multiplication = BigInt(num1 * num2);
-console.log(multiplication)
-alert(multiplication);
-console.log(multiplication);
+// const num1 = BigInt(prompt())
+// console.log(num1);
+// const num2 = BigInt(prompt())
+// console.log(num2)
+// const multiplication = BigInt(num1 * num2);
+// console.log(multiplication)
+// alert(multiplication);
+// console.log(multiplication);
