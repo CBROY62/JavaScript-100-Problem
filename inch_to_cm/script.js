1e5cfadfba31a028bbf3451306663e075a42ad85
CBROY62 <chandrabhushankumar27080@gmail.com>
2025-07-10 22:53:46 +0530

    add some of data

---
+++ inch_to_cm/script.js
@@ -0,0 +1,20 @@
+//var kg = pounds * 0.453592;
+
+function calculate() {
+  const inputValue = document.querySelector(".Inpute_section");
+  let screenValue = inputValue.value;
+  console.log(screenValue);
+
+  const button_section = document.querySelector(".calculate_section");
+  let button_section1 = button_section.value;
+  console.log(button_section1);
+
+  button_section.addEventListener("click", calculate);
+
+
+  const calValue = screenValue * 2.54;
+  console.log(calValue);
+
+  let peramiterValue = document.querySelector(".final_kg_calculated_value");
+  peramiterValue.textContent = `weight cm = ${calValue}`;
+}
