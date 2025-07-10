8aad72247370a678cf580af97450dbe570dec0c3
CBROY62 <chandrabhushankumar27080@gmail.com>
2025-07-10 12:44:18 +0530

    add some of data

---
+++ WeightConvertor/Script.js
@@ -0,0 +1,19 @@
+//var kg = pounds * 0.453592;
+
+function calculate() {
+  const inputValue = document.querySelector(".Inpute_section");
+  let screenValue = inputValue.value;
+  console.log(screenValue);
+
+  const button_section = document.querySelector(".calculate_section");
+  let button_section1 = button_section.value;
+  console.log(button_section1);
+
+  button_section.addEventListener("click", calculate);
+
+  const calValue = screenValue * 0.45;
+  console.log(calValue);
+
+  let peramiterValue = document.querySelector(".final_kg_calculated_value");
+  peramiterValue.textContent = `weight KG = ${calValue}`;
+}
